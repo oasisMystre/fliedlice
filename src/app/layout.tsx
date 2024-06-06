e850abf4b4c3f4d0836302a6c0b8c5ae5101b053
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-06-06 03:48:34 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,10 @@ export const metadata: Metadata = {
   title: "FliedLice | Stir frying your investments",
   description:
     "Discover the delightful world of Fliedlice, the latest meme coin sensation that's taking the crypto market by storm. Join the Fliedlice community today and stir up your investments with a smile.",
+metadataBase: new URL("https://fliedlice.xyz"),
+openGraph: {
+    images: "/banner.png",
+  },
 };
 
 export default function RootLayout({
